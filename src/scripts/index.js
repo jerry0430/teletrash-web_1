adf62f446f08bc0226bac484c787fb31ba72a7c8
Jerry 張乃文 <jerry10jerry10@gmail.com>
2019-09-19 15:46:20 +0800

    切版v2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -25,11 +25,11 @@ const rankBar = document.getElementsByClassName('rank__wrap__color')
 for(let i = 0; i<rankPersent.length; i++){
 	let rPersent = parseInt(rankPersent[i].innerText.replace('%', ''))
 	rankBar[i].style.width = `${rPersent}%`
-	if(rPersent <= 50){
-		rankBar[i].style.backgroundColor = '#ea6767'
-	} else if(rPersent > 50 && rPersent < 80){
+	if(rPersent < 50){
+		rankBar[i].style.backgroundColor = '#189f96'
+	} else if(rPersent > 49 && rPersent < 69){
 		rankBar[i].style.backgroundColor = '#fdac2c'
 	} else {
-		rankBar[i].style.backgroundColor = '#179f96'
+		rankBar[i].style.backgroundColor = '#ea6767'
 	}
 }
